6c4024ef565cc3be6f2ad216ef62d705842e7f22
ElementalAlchemist <ElementAlchemist7@gmail.com>
2024-12-24 15:03:08 -0600

    Revisit day 23 part 2
    When I first submitted day 23, I was a bit surprised that the algorithm I had got the answer, given I expected it would veer into a wrong answer early and be unrecoverable, but it did manage to get the answer to the problem.
    
    However, there are inputs for which it isn't as lucky and doesn't work. Since then, I've learned of another algorithm, the Bron-Kerbosch algorithm, which handles this quite well and more correctly. I wanted to have a go at fixing my day 23 submission, so I did.

--- d23p2/src/main.rs
+++ d23p2/src/main.rs
@@ -1,39 +1,69 @@
-use std::collections::{BTreeSet, HashMap, HashSet};
+use std::collections::{HashMap, HashSet};
 use std::error::Error;
 use std::fs;
+
+fn find_maximal_clique(connections: &HashMap<String, HashSet<String>>) -> HashSet<String> {
+	let to_be_considered: HashSet<String> = connections.keys().cloned().collect();
+	sub_find_maximal_clique(connections, &to_be_considered, &HashSet::new(), &HashSet::new()).unwrap()
+}
+
+fn sub_find_maximal_clique(
+	connections: &HashMap<String, HashSet<String>>,
+	to_be_considered: &HashSet<String>,
+	in_group: &HashSet<String>,
+	out_of_group: &HashSet<String>,
+) -> Option<HashSet<String>> {
+	if to_be_considered.is_empty() && out_of_group.is_empty() {
+		return Some(in_group.clone());
+	}
+	let mut considering = to_be_considered.clone();
+	let mut out_of_group = out_of_group.clone();
+	let empty_set = HashSet::new();
+	let mut biggest_clique: Option<HashSet<String>> = None;
+	for vertex in to_be_considered.iter() {
+		let mut vertex_in_group = in_group.clone();
+		vertex_in_group.insert(vertex.clone());
+		let vertex_connections = connections.get(vertex).unwrap_or(&empty_set);
+		let vertex_considering: HashSet<String> = considering.intersection(vertex_connections).cloned().collect();
+		let vertex_out_of_group: HashSet<String> = out_of_group.intersection(vertex_connections).cloned().collect();
+		let max_clique =
+			sub_find_maximal_clique(connections, &vertex_considering, &vertex_in_group, &vertex_out_of_group);
+		if let Some(clique) = max_clique {
+			match &mut biggest_clique {
+				Some(old_biggest) => {
+					if clique.len() > old_biggest.len() {
+						*old_biggest = clique;
+					}
+				}
+				None => biggest_clique = Some(clique),
+			}
+		}
+		considering.remove(vertex);
+		out_of_group.insert(vertex.clone());
+	}
+	biggest_clique
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
 	let connections = {
 		let input_string = fs::read_to_string("input.txt")?;
 
-		let mut connections: HashMap<String, BTreeSet<String>> = HashMap::new();
+		let mut connections: HashMap<String, HashSet<String>> = HashMap::new();
 		for line in input_string.lines() {
 			let (one_computer, other_computer) =
 				line.split_once('-').expect("Each line should have a pair of computers");
 			let one_connection = connections.entry(one_computer.to_string()).or_default();
-			one_connection.insert(one_computer.to_string());
 			one_connection.insert(other_computer.to_string());
 			let other_connection = connections.entry(other_computer.to_string()).or_default();
-			other_connection.insert(other_computer.to_string());
 			other_connection.insert(one_computer.to_string());
 		}
 		connections
 	};
 
-	let mut groups: HashSet<BTreeSet<String>> = HashSet::new();
-	for grouped_computers in connections.values() {
-		let mut connection_group = BTreeSet::new();
-		for computer in grouped_computers.iter() {
-			if connection_group.is_subset(connections.get(computer).unwrap()) {
-				connection_group.insert(computer.clone());
-			}
-		}
-		groups.insert(connection_group);
-	}
+	let biggest_group = find_maximal_clique(&connections);
 
-	let max_group_size = groups.iter().map(|group| group.len()).max().unwrap();
-	let biggest_group = groups.iter().find(|group| group.len() == max_group_size).unwrap();
-	let biggest_group: Vec<String> = biggest_group.iter().cloned().collect();
+	let mut biggest_group: Vec<String> = biggest_group.into_iter().collect();
+	biggest_group.sort_unstable();
 	let password = biggest_group.join(",");
 	println!("{}", password);
 
